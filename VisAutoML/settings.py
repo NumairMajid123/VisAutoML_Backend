2876d04a0da37ded31d97c7fe3456712bd4250ff
Siddiqui-bit <113021834+Siddiqui-bit@users.noreply.github.com>
2025-04-02 01:27:15 +0500

    updated settings.py

--- VisAutoML/settings.py
+++ VisAutoML/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = 'django-insecure-hzpyx0#-@0+j(ml=y4gy29pzr2kv9jq4#-x-cf95m_veno7avq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
